944e1c70f3f37c421b6dfe4e59908b367ce77a3e
Dhanajit kapali <dhanajitkapali30@gmail.com>
2021-08-10 11:31:43 +0530

    added code to change the app icon dynamically. Found issue:- App icon not changing.

--- DynamicAppIcon/ViewController.swift
+++ DynamicAppIcon/ViewController.swift
@@ -8,12 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var changeAppIconButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func changeAppIconButtonPressed(_ sender: UIButton) {
+        print("Changing the App Icon")
+        if UIApplication.shared.supportsAlternateIcons{
+            UIApplication.shared.setAlternateIconName("AppIcon-3") { error in
+                if let error = error {
+                    print(error.localizedDescription)
+                    print(UIApplication.shared.alternateIconName ?? "Primary")
+                } else {
+                    print("Successfully Changed the App Icon")
+                    print(UIApplication.shared.alternateIconName ?? "Primary")
+                }
+            }
+        }else{
+            print("App doesn't support alternate icon")
+            print(UIApplication.shared.alternateIconName ?? "Primary")
+        }
+        
+    }
+    
 }
 
