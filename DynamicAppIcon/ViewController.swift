7c71bfa6062df86787b5653a4f905c118763efd5
Dhanajit kapali <dhanajitkapali30@gmail.com>
2021-08-10 12:20:28 +0530

    added two buttons to switch the icons

--- DynamicAppIcon/ViewController.swift
+++ DynamicAppIcon/ViewController.swift
@@ -9,14 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet var changeAppIconButton: UIButton!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    @IBAction func changeAppIconButtonPressed(_ sender: UIButton) {
+    @IBAction func changeAppIconButtonPressed1(_ sender: UIButton) {
         print("Changing the App Icon")
         if UIApplication.shared.supportsAlternateIcons{
             //"AppIcon-2" is a reference name for the Icon file name, its defined in info.plist.
@@ -35,7 +33,28 @@ class ViewController: UIViewController {
             print(UIApplication.shared.alternateIconName ?? "Primary")
         }
         
-    }
+    } //:changeAppIconButtonPressed1
+    
+    @IBAction func changeAppIconButtonPressed2(_ sender: UIButton) {
+        print("Changing the App Icon")
+        if UIApplication.shared.supportsAlternateIcons{
+            //"AppIcon-2" is a reference name for the Icon file name, its defined in info.plist.
+            UIApplication.shared.setAlternateIconName("AppIcon-3") { error in
+                if let error = error {
+                    print(error.localizedDescription)
+                    //getting the name of the current alternate icon
+                    print(UIApplication.shared.alternateIconName ?? "Primary")
+                } else {
+                    print("Successfully Changed the App Icon")
+                    print(UIApplication.shared.alternateIconName ?? "Primary")
+                }
+            }
+        }else{
+            print("App doesn't support alternate icon")
+            print(UIApplication.shared.alternateIconName ?? "Primary")
+        }
+        
+    } //:changeAppIconButtonPressed2
     
 }
 
